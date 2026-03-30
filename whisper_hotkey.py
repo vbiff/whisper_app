188837cd8ef73a146db2b452d5e5f98b32165c84
Valera <Valeraclawd@OpenClawds-MacBook-Pro.local>
2026-03-30 11:45:11 +0100

    fix: clean exit on Ctrl+C

--- whisper_hotkey.py
+++ whisper_hotkey.py
@@ -93,6 +93,9 @@ def transcribe():
 
 
 # Запуск
-with sd.InputStream(samplerate=SAMPLE_RATE, channels=1, callback=audio_callback):
-    with keyboard.Listener(on_press=on_press, on_release=on_release, suppress=True) as listener:
-        listener.join()
+try:
+    with sd.InputStream(samplerate=SAMPLE_RATE, channels=1, callback=audio_callback):
+        with keyboard.Listener(on_press=on_press, on_release=on_release, suppress=True) as listener:
+            listener.join()
+except KeyboardInterrupt:
+    print("\n👋 Bye!")
